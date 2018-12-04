59ecb720491caa5523b1dbb2841753b9a75f26c8
mfosho <mc8160@gmail.com>
2018-12-04 10:58:10 -0500

    changed database connection

--- database/sql/knexConnection.js
+++ database/sql/knexConnection.js
@@ -4,7 +4,7 @@ const pg = require('pg');
 var knex = require('knex')({
   client: 'pg',
   connection: {
-    host     : '3.16.27.159',
+    host     : 'ec2-18-223-209-234.us-east-2.compute.amazonaws.com',
     user     : 'postgres',
     password : '0237100t',  
     database : 'staybnb_knex',
